4d1e4ccc08edbe9ec80876b91f7c8c4064053228
Alex Panchenko <440271+panchenko@users.noreply.github.com>
2025-07-01 15:16:58 +0200

    startAndStopProxyServer method extracted

--- src/test/java/org/littleshoot/proxy/impl/ServerGroupTest.java
+++ src/test/java/org/littleshoot/proxy/impl/ServerGroupTest.java
@@ -10,19 +10,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class ServerGroupTest {
     private ServerGroup serverGroup;
 
+    private void startAndStopProxyServer() {
+        HttpProxyServer proxyServer = DefaultHttpProxyServer.bootstrap().withServerGroup(serverGroup).start();
+        proxyServer.stop();
+    }
+
     @Test
     void autoStop() {
         serverGroup = new ServerGroup("Test", 4, 4, 4);
-        HttpProxyServer proxyServer = DefaultHttpProxyServer.bootstrap().withServerGroup(serverGroup).start();
-        proxyServer.stop();
+        startAndStopProxyServer();
         assertTrue(serverGroup.isStopped(), "server group stopped");
     }
 
     @Test
     void manualStop() {
         serverGroup = new ServerGroup("Test", 4, 4, 4, false);
-        HttpProxyServer proxyServer = DefaultHttpProxyServer.bootstrap().withServerGroup(serverGroup).start();
-        proxyServer.stop();
+        startAndStopProxyServer();
         assertFalse(serverGroup.isStopped(), "server group stopped");
     }
 
