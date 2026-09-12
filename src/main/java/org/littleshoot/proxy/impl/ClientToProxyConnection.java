5460ec920fe891152298cb122c9f0f0bc88e7c92
Charles Lescot <910188+clescot@users.noreply.github.com>
2026-09-12 02:26:17 +0200

    import main into the branch

--- src/main/java/org/littleshoot/proxy/impl/ClientToProxyConnection.java
+++ src/main/java/org/littleshoot/proxy/impl/ClientToProxyConnection.java
@@ -12,6 +12,7 @@ import static org.littleshoot.proxy.impl.ConnectionState.AWAITING_PROXY_AUTHENTI
 import static org.littleshoot.proxy.impl.ConnectionState.DISCONNECT_REQUESTED;
 import static org.littleshoot.proxy.impl.ConnectionState.NEGOTIATING_CONNECT;
 
+import com.github.f4b6a3.ulid.UlidCreator;
 import com.google.errorprone.annotations.CheckReturnValue;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -1695,9 +1696,17 @@ public class ClientToProxyConnection extends ProxyConnection<HttpRequest> {
         @Override
         protected void requestRead(HttpRequest httpRequest) {
           recordClientConnected();
+          String requestId = UlidCreator.getMonotonicUlid().toString();
+          if (ctx != null && ctx.channel() != null) {
+            ctx.channel().attr(REQUEST_ID_KEY).set(requestId);
+          }
           FlowContext flowContext = flowContext();
           for (ActivityTracker tracker : proxyServer.getActivityTrackers()) {
-            tracker.requestReceivedFromClient(flowContext, httpRequest);
+            try {
+              tracker.requestReceivedFromClient(flowContext, httpRequest, requestId);
+            } catch (Exception e) {
+              LOG.error("Unable to requestReceivedFromClient", e);
+            }
           }
         }
       };
@@ -1717,9 +1726,17 @@ public class ClientToProxyConnection extends ProxyConnection<HttpRequest> {
       new ResponseWrittenMonitor() {
         @Override
         protected void responseWritten(HttpResponse httpResponse) {
+          String requestId = null;
+          if (ctx != null && ctx.channel() != null) {
+            requestId = ctx.channel().attr(REQUEST_ID_KEY).get();
+          }
           FlowContext flowContext = flowContext();
           for (ActivityTracker tracker : proxyServer.getActivityTrackers()) {
-            tracker.responseSentToClient(flowContext, httpResponse);
+            try {
+              tracker.responseSentToClient(flowContext, httpResponse, requestId);
+            } catch (Exception e) {
+              LOG.error("Unable to write response", e);
+            }
           }
         }
       };
